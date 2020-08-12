fd1de8baec71daa3b57f1ac657464ef372be355d
Raul Perusquia <raul@inikoo.com>
2020-08-12 10:43:27 +0800

    moving files

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+/*
+ Author: Raul A Perusquía-Flores (raul@aiku.io)
+ Created: Wed, 12 Aug 2020 00:32:45 Malaysia Time, Kuala Lumpur, Malaysia
+ Copyright (c) 2020. Aiku.io
+ */
+
+import React from 'react';
+
+
+const Dashboard = () => {
+
+    return (<div>Dashboard</div>);
+
+
+};
+
+export default Dashboard;
