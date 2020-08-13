df27f13588178f548cbb33a0a79dade7389514b1
kohani-mohan <kohani@aiku.io>
2020-08-13 12:14:02 +0800

    add the mobile version side navigation.

--- src/components/navigation/MobileSideNav.js
+++ src/components/navigation/MobileSideNav.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import {
+    Link
+} from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTachometerAlt, faPeopleCarry, faProjectDiagram, faCalendarAlt, faFolders, faUsers, faMailBulk, faStoreAlt, faBadgePercent,faGlobe,faShoppingCart,faConveyorBeltAlt,faWarehouseAlt,faBox,faHandHoldingBox,faIndustry,faClipboardUser,faAbacus,faChartLine,faUsersClass,faSlidersH} from '@fortawesome/pro-regular-svg-icons'
 
+
+
+const MobileSideNavLink = (props) => {
+
+
+    return (<Link to={props.to}
+                  className={`${props.selected ? 'text-white bg-gray-0' : 'text-gray-300 hover:text-white hover:bg-gray-700 focus:text-white'} 
+               group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:outline-none transition ease-in-out duration-150 focus:bg-gray-700`}>
+        <FontAwesomeIcon
+            className="fa-fw mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
+            icon={props.icon}/>
+        {props.text}
+    </Link>)
+
+}
 
 const MobileSideNav = () => {
 
@@ -8,11 +28,9 @@ const MobileSideNav = () => {
 
         <div className="md:hidden">
             <div className="fixed inset-0 flex z-40">
-
                 <div className="fixed inset-0">
                     <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
                 </div>
-
                 <div className="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800">
                     <div className="absolute top-0 right-0 -mr-14 p-1">
                         <button className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" aria-label="Close sidebar">
@@ -25,61 +43,34 @@ const MobileSideNav = () => {
                         <div className="flex-shrink-0 flex items-center px-4">
                             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg" alt="Workflow"/>
                         </div>
+
+
                         <nav className="mt-5 px-2 space-y-1">
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
-                                </svg>
-                                Dashboard
-                            </a>
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
-                                </svg>
-                                Team
-                            </a>
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
-                                </svg>
-                                Projects
-                            </a>
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                </svg>
-                                Calendar
-                            </a>
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"/>
-                                </svg>
-                                Documents
-                            </a>
-                            <a href="#"
-                               className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150">
-                                <svg className="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
-                                </svg>
-                                Reports
-                            </a>
+                            <MobileSideNavLink to="/dashboard" text="Dashboard" icon={faTachometerAlt}/>
+                            <MobileSideNavLink to="/team" text="Team" icon={faPeopleCarry}/>
+                            <MobileSideNavLink to="/projects" text="Projects" icon={faProjectDiagram}/>
+                            <MobileSideNavLink to="/calendar" text="Calendar" icon={faCalendarAlt}/>
+                            <MobileSideNavLink to="/documents" text="Documents" icon={faFolders}/>
+                            <MobileSideNavLink to="/customers" text="Customers" icon={faUsers} selected={true}/>
+                            <MobileSideNavLink to="/mailroom" text="Mailroom" icon={faMailBulk}/>
+                            <MobileSideNavLink to="/products" text="Products" icon={faStoreAlt}/>
+                            <MobileSideNavLink to="/offers" text="Offers" icon={faBadgePercent}/>
+                            <MobileSideNavLink to="/websites" text="Websites" icon={faGlobe}/>
+                            <MobileSideNavLink to="/orders" text="Orders" icon={faShoppingCart}/>
+                            <MobileSideNavLink to="/delivering" text="Delivering" icon={faConveyorBeltAlt}/>
+                            <MobileSideNavLink to="/warehouse" text="Warehouse" icon={faWarehouseAlt}/>
+                            <MobileSideNavLink to="/inventory" text="Inventory" icon={faBox}/>
+                            <MobileSideNavLink to="/suppliers" text="Suppliers" icon={faHandHoldingBox}/>
+                            <MobileSideNavLink to="/production" text="Production" icon={faIndustry}/>
+                            <MobileSideNavLink to="/staff" text="Staff" icon={faClipboardUser}/>
+                            <MobileSideNavLink to="/accounting" text="Accounting" icon={faAbacus}/>
+                            <MobileSideNavLink to="/reports" text="Reports" icon={faChartLine}/>
+                            <MobileSideNavLink to="/users" text="Users" icon={faUsersClass}/>
+                            <MobileSideNavLink to="/settings" text="Settings" icon={faSlidersH}/>
                         </nav>
                     </div>
+
+
                     <div className="flex-shrink-0 flex bg-gray-700 p-4">
                         <a href="#" className="flex-shrink-0 group block">
                             <div className="flex items-center">
