e7e76f206cf30e63bfcffc03c60310f59a295adb
Raul Perusquia <raul@inikoo.com>
2020-08-18 00:02:58 +0800

    Language select button scaffolding

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -6,13 +6,22 @@
  */
 
 import React from 'react';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faLanguage} from '@fortawesome/pro-light-svg-icons'
 
 const Footer = () => {
 
     return (
 
-        <footer className="bg-gray-800 h-6 text-gray-200">
+        <footer className="flex justify-end bg-gray-800 h-6 text-gray-300">
+
+                    <button className="mr-5 group flex items-center  ">
+                    <FontAwesomeIcon
+                        className="text-lg fa-flip-horizontal mr-2 h-6 w-6 group-hover:text-gray-200 group-focus:text-gray-200 transition ease-in-out duration-150 "
+                        icon={faLanguage}/>
+                        <span className="group-hover:text-gray-200 group-focus:text-gray-200 transition ease-in-out duration-150">English</span>
+                    </button>
+
         </footer>
     );
 
