748961e91d80ff8d52e8eebb0ca8772ab26316b3
Raul Perusquia <raul@inikoo.com>
2020-08-21 14:24:32 +0800

    fix users table

--- src/components/system/Users.js
+++ src/components/system/Users.js
@@ -52,17 +52,22 @@ const USERS = gql`
 `;
 
 
-function EmployeesTable() {
+function UsersTable() {
 
     const {loading, error, data} = useQuery(USERS);
     if (loading) return <p><Trans>Loading...</Trans></p>;
     if (error) return <p><Trans>Error</Trans> :(</p>;
 
-    const res = data['employees'];
+    const res = data['users'];
     const headers = [<Trans>User</Trans>, <Trans>title</Trans>, <Trans>Status</Trans>, <Trans>Role</Trans>, ''];
 
+
+console.log(res.data)
+
     const users = res.data.map(obj => {
-        return [<Link to={'/system/users/' + obj.handle}><AvatarCell name={obj["userable"].name} property={obj.handle}/></Link>, <TwoLineCell main={obj["userable"].name} secondary={obj.handle}/>,
+        return [
+            <Link to={'/system/users/' + obj.handle}><AvatarCell slug={obj.handle} name={obj.handle}/></Link>,
+            <TwoLineCell main={obj["userable"].name} secondary={obj.handle}/>,
 
         ]
     })
@@ -84,7 +89,7 @@ const Users = () => {
             metas={[]}
             actions={actions}
         />
-        <EmployeesTable/>
+        <UsersTable/>
 
     </div>);
 };
