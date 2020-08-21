99ec4a12406f5add2c28d8aebbd292329b64c451
Raul Perusquia <raul@inikoo.com>
2020-08-21 16:07:39 +0800

    edit model

--- src/components/ui/forms/Form.js
+++ src/components/ui/forms/Form.js
@@ -63,7 +63,7 @@ const Form = (props) => {
         <div className="mt-8 border-t border-gray-200 pt-5">
             <div className="flex justify-end">
                 <span className="inline-flex rounded-md shadow-sm">
-        <button type="button"
+        <button onClick={props.handleCancel} type="button"
                 className="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
             <Trans>Cancel</Trans>
         </button>
