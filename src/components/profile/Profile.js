f0b69f5ff74aba523236566a45ab296bf400e12b
kohani-mohan <kohani@aiku.io>
2020-08-24 13:42:01 +0800

    Create edit function in profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,111 +1,239 @@
 import React from 'react';
+import {gql, useMutation, useQuery} from "@apollo/client";
+import {Trans} from "@lingui/macro";
+import HeaderMetaActions from "../ui/headers/HeaderMetaActions";
+import {useParams} from "react-router";
+import {faIdCardAlt, faPencilAlt} from '@fortawesome/pro-solid-svg-icons'
+import Input from "../ui/forms/fields/Input";
+import {i18nMark} from "@lingui/react";
+import Form from "../ui/forms/Form";
 
 
-const Profile = () => {
 
-    return (<div className="lg:flex lg:items-center lg:justify-between pt-4">
-        <div className="flex-1 min-w-0">
-            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
-                My profile
-            </h2>
-            <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
-                <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
-                    <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd"
-                              d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                              clipRule="evenodd"/>
-                        <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"/>
-                    </svg>
-                    Full-time
-                </div>
-                <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
-                    <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
-                    </svg>
-                    Remote
-                </div>
-                <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
-                    <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path
-                            d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"/>
-                        <path fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-                              clipRule="evenodd"/>
-                    </svg>
-                    $120k &ndash; $140k
-                </div>
-                <div className="mt-2 flex items-center text-sm leading-5 text-gray-500">
-                    <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                              clipRule="evenodd"/>
-                    </svg>
-                    Closing on January 9, 2020
+
+function ProfileShowcase(props) {
+
+    let {userHandle} = useParams();
+    const [updateUser] = useMutation(UPDATE_USER);
+
+
+    const {loading, error, data} = useQuery(USER, {
+        variables: {userHandle},
+
+    });
+    if (loading) return <p><Trans>Loading...</Trans></p>;
+    if (error) return <p><Trans>Error</Trans> :(</p>;
+
+
+    let metas = [];
+    let actions = [];
+
+
+    if (!props.editing) {
+        actions.push({
+            'icon': faPencilAlt, 'label': <Trans>Edit Password</Trans>, 'highlighted': false, handleClick: props.openEditViewPassword
+
+        });
+    }
+
+    if (!props.editing) {
+        actions.push({
+            'icon': faPencilAlt, 'label': <Trans>Edit Pin</Trans>, 'highlighted': false, handleClick: props.openEditViewPin
+
+        });
+    }
+
+
+    switch (data.user['userable'].__typename) {
+        case 'Employee':
+            metas.push({
+                'icon': faIdCardAlt, 'label': <Trans>{data.user['userable'].name} (Employee)</Trans>
+            });
+            break;
+        default:
+            //
+            break;
+
+    }
+
+
+    const formPasswordStructure = {
+
+        handleCancel: props.cancelEditPassword,
+
+        handleSubmit: updateUser,
+
+        modelID: {name: 'id', value: data['user'].id},
+
+
+        inputGroups: [{
+
+            key: 'identification',
+
+            title: <Trans>Change Password</Trans>,
+
+            note: <Trans></Trans>,
+
+            fields: [
+
+                {
+                    key: 'password',
+
+                    label: <Trans>Password</Trans>,
+
+                    inputComponent: <Input
+
+                        name='password'
+                        placeholder={i18nMark('password')}
+                        hint='&nbsp;'
+                        value={''}
+                        register={{
+                            required: <Trans>This is required.</Trans>, minLength: {
+                                value: 6, message: <Trans>This input requires minimum {6} characters.</Trans>
+                            }
+                        }}
+                    />
+                },
+
+            ],
+
+
+        }]
+    }
+
+    const formPinStructure = {
+
+        handleCancel: props.cancelEditPin,
+
+        handleSubmit: updateUser,
+
+        modelID: {name: 'id', value: data['user'].id},
+
+
+        inputGroups: [{
+
+            key: 'identification',
+
+            title: <Trans>Change Pin</Trans>,
+
+            note: <Trans></Trans>,
+
+            fields: [
+
+                {
+
+                    key: 'pin',
+
+                    label: <Trans>Pin</Trans>,
+
+                    inputComponent: <Input
+
+                        name='pin'
+                        placeholder={i18nMark('pin')}
+                        hint='&nbsp;'
+                        value={''}
+                        register={{
+                            required: <Trans>This is required.</Trans>, minLength: {
+                                value: 4, message: <Trans>This input requires minimum {4} characters.</Trans>
+                            }
+                        }}
+                    />
+                }
+
+
+            ],
+
+
+        }]
+    }
+
+
+    return (<><HeaderMetaActions
+        title={data.user.handle}
+        metas={metas}
+        actions={actions}
+
+    />
+        {props.editingPassword ? <Form {...formPasswordStructure} /> : null}
+        {props.editingPin? <Form {...formPinStructure} /> : null}
+
+    </>)
+
+}
+
+    const Profile = () => {
+
+
+        const [editingPassword, setEditingPassword] = React.useState(false);
+
+        const openEditViewPassword = () => {
+            setEditingPassword(true);
+        }
+        const cancelEditPassword = () => {
+            setEditingPassword(false);
+        }
+
+        const [editingPin, setEditingPin] = React.useState(false);
+
+        const openEditViewPin = () => {
+            setEditingPin(true);
+        }
+        const cancelEditPin = () => {
+            setEditingPin(false);
+        }
+
+
+        return (<>
+
+            <ProfileShowcase editingPassword={editingPassword} openEditViewPassword={openEditViewPassword} cancelEditPassword={cancelEditPassword}
+
+                          editingPin={editingPin} openEditViewPin={openEditViewPin} cancelEditPin={cancelEditPin}/>
+
+
+        </>);
+
+        return (<div className="lg:flex lg:items-center lg:justify-between pt-4">
+            <div className="flex-1 min-w-0">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+                    My profile
+                </h2>
+                <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
+                    <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
+                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd"
+                                  d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                                  clipRule="evenodd"/>
+                            <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"/>
+                        </svg>
+                        Full-time
+                    </div>
+                    <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
+                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
+                        </svg>
+                        Remote
+                    </div>
+                    <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mr-6">
+                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"/>
+                            <path fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                                  clipRule="evenodd"/>
+                        </svg>
+                        $120k &ndash; $140k
+                    </div>
+                    <div className="mt-2 flex items-center text-sm leading-5 text-gray-500">
+                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                  clipRule="evenodd"/>
+                        </svg>
+                        Closing on January 9, 2020
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="mt-5 flex lg:mt-0 lg:ml-4">
-    <span className="hidden sm:block shadow-sm rounded-md">
-      <button type="button"
-              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out">
-        <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>
-        </svg>
-        Edit
-      </button>
-    </span>
 
-            <span className="hidden sm:block ml-3 shadow-sm rounded-md">
-      <button type="button"
-              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-        <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd"
-                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-                clipRule="evenodd"/>
-        </svg>
-        View
-      </button>
-    </span>
-
-            <span className="sm:ml-3 shadow-sm rounded-md">
-      <button type="button"
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out">
-        <svg className="-ml-1 mr-2 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"/>
-        </svg>
-        Publish
-      </button>
-    </span>
-
-            {/* Dropdown */}
-            <span className="ml-3 relative shadow-sm rounded-md sm:hidden">
-      <button type="button"
-              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out"
-              id="mobile-menu" aria-haspopup="true">
-        More
-        <svg className="-mr-1 ml-2 h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"/>
-        </svg>
-      </button>
-
-                {/*
-                  Dropdown panel, show/hide based on dropdown state.
-
-                  Entering: "transition ease-out duration-200"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                */}
-                <div className="origin-top-right absolute right-0 mt-2 -mr-1 w-48 rounded-md shadow-lg" aria-labelledby="mobile-menu" role="menu">
-        <div className="py-1 rounded-md bg-white shadow-xs">
-          <a href="#" className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">Edit</a>
-        </div>
-      </div>
-    </span>
-        </div>
-    </div>);
-
+        </div>);
 
 };
 
